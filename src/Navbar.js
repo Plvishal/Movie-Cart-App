4f61e220f689756def1126bf2c17835d99ab479e
Vishal <vishalpal1501@gmail.com>
2023-10-30 22:36:07 +0530

    create a variable style just below the export of the page and just pass the reference of the style{style.nav}

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,20 +4,16 @@ class Navbar extends React.Component {
   render() {
     return (
       <>
-        <div
-          style={{
-            width: '100%',
-            height: '70px',
-            background: 'red',
-            display: 'flex',
-            justifyContent: 'space-between',
-          }}
-        >
-          <div>Title</div>
-          <div>
-            <img src="" alt="CartIcon" />
+        <div style={styles.nav}>
+          <div style={styles.title}>Movie -App</div>
+          <div style={styles.cartContainer}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
+              alt="CartIcon"
+              style={styles.cartIcon}
+            />
 
-            <span>0</span>
+            <span style={styles.cartCount}>0</span>
           </div>
         </div>
       </>
@@ -26,3 +22,36 @@ class Navbar extends React.Component {
 }
 
 export default Navbar;
+const styles = {
+  nav: {
+    width: '100%',
+    height: '70px',
+    background: '#dc2430',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    color: 'white',
+    fontSize: '30px',
+    textTransform: 'uppercase',
+    marginLeft:'15px'
+  },
+  cartContainer: {
+    position: 'relative',
+    cursor: 'pointer',
+  },
+  cartIcon: {
+    height: '48px',
+    marginRight: '20px',
+  },
+  cartCount: {
+    background: 'orange',
+    borderRadius: '50%',
+    padding: '4px 8px',
+    position: 'absolute',
+    right: '10px',
+    top: '-5px',
+    fontSize: '12px',
+  },
+};
