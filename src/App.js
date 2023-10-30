67ed398c8fba3533db830afc952cf4a197585093
Vishal <vishalpal1501@gmail.com>
2023-10-30 21:35:52 +0530

    Navbar only div conatiner define

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import MovieList from './MovieList';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <h1> Movie App</h1>
       <MovieList />
     </>
---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <>
+        <div>
+          <div>Title</div>
+          <div>
+            <div>
+              <img src="" alt="CartIcon" />
+            </div>
+            <div>
+              <span>0</span>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Navbar;
