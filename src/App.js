6239e46934529da326db87cdf0fa96ee20c2954b
Vishal <vishalpal1501@gmail.com>
2023-11-01 23:27:08 +0530

    Lifting state in React  and also separate Movie Data inside movieData.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,81 @@
+import React from 'react';
 import MovieList from './MovieList';
 import Navbar from './Navbar';
+import { movies } from './movieData';
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <h1> Movie App</h1>
-      <MovieList />
-    </>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    //Creating the state object
+    this.state = {
+      movies: movies,
+      cartCount: 0,
+    };
+  }
+
+  handleAddStars = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    if (movies[movieId].stars < 5) {
+      movies[movieId].stars += 0.5;
+    }
+
+    this.setState({
+      movies,
+    });
+  };
+
+  handleDecStars = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    if (movies[movieId].stars > 0) {
+      movies[movieId].stars -= 0.5;
+    }
+
+    this.setState({
+      movies,
+    });
+  };
+
+  handleToggleFav = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    movies[movieId].fav = !movies[movieId].fav;
+
+    this.setState({
+      movies,
+    });
+  };
+
+  handleAddtocart = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    movies[movieId].isInCart = !movies[movieId].isInCart;
+
+    this.setState({
+      movies,
+    });
+  };
+  render() {
+    const { movies } = this.state;
+    return (
+      <>
+        <Navbar />
+        <h1> Movie App</h1>
+        <MovieList
+          movies={movies}
+          onIncStars={this.handleAddStars}
+          onDecStars={this.handleDecStars}
+          onClickFav={this.handleToggleFav}
+          onClickAddtocart={this.handleAddtocart}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
